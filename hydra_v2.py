8f444fde5794c7a195617959aba8d0c36a806ac5
torall <ryanlewis1@gmail.com>
2015-04-15 21:42:02 -0400

    new

---
+++ hydra_v2.py
@@ -0,0 +1,60 @@
+import os
+
+# Get user info
+who = (os.popen('whoami').readline())
+
+def main_menu():
+    os.system('clear')
+    menu = 'main'
+
+    print 'Hello, ', who
+    print '1. Shows'
+    print '0. Quit'
+
+    input = raw_input(' >> ')
+    exec_menu(input,menu)
+
+
+def movies():
+    os.system('clear')
+    menu = 'movies'
+
+def shows():
+    os.system('clear')
+    menu = 'shows'
+    print '1. List'
+    print '2. Add'
+    print '3. Remove'
+    print '4. Request'
+
+    input = raw_input(' >> ')
+    exec_menu(input,menu)
+
+
+def exec_menu(input, menu):
+
+    if menu == 'main' and input == '1':
+        shows()
+    if menu == 'shows' and input == '1':
+        list_user_shows(menu)
+    if menu =='shows' and input == '2':
+        add_show(menu)
+
+
+
+# List user shows
+def list_user_shows(menu):
+    print 'Here is a list of shows on your profile'
+    input = raw_input(' >> ')
+    exec_menu(input,menu)
+
+
+# Add a show to the users node
+def add_show(menu):
+    show = raw_input('Please enter the name of the show: ')
+    print ('you have entered, '), show
+    
+
+# Main
+if __name__ == "__main__":
+    main_menu()
