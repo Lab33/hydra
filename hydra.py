8165d9a854c63deb47b61b8f6518d50078cf9c3c
Lab33 <ryanlewis1@gmail.com>
2015-04-21 22:09:35 -0400

    File transfer and folder delete now works

--- hydra.py
+++ hydra.py
@@ -9,6 +9,11 @@ import datetime
 
 
 
+#########################################################
+####### TO DO
+# - if the folder has a space the process will fail
+# - replace('.',' ') doesnt work with Marvel, going to have to add a special rule
+
 regex = '[sS]+[0-9]+[eE]+[0-9]+[0-9]' # expression to find shows
 filelist = []
 configLocName = 'hydra.conf'
@@ -22,6 +27,7 @@ node = ''
 Config = ConfigParser.ConfigParser()
 Config.read(configLocName) 
 Config.sections()
+   
 
 def ConfigSectionMap(section):
     dict1 = {}
@@ -44,14 +50,21 @@ user = ConfigSectionMap("Database")['user']
 password = ConfigSectionMap("Database")['pass']
 db = ConfigSectionMap("Database")['db']
 debug = ConfigSectionMap("General")['debug']
-servername = ConfigSectionMap("NAS"),['servername']
-#torrentfolder = ConfigSectionMap("NAS"),['torrentfolder']
-#showfolder = ConfigSectionMap("NAS"),['showfolder']
+server_name = ConfigSectionMap("NAS")['servername']
+torrentfolder = ConfigSectionMap("NAS")['torrentfolder']
+showfolder = ConfigSectionMap("NAS")['showfolder']
 
 #if debug == '1':
-print provider, host, user, password, db, debug#, servername
-#print 'TorrentFolder: ', torrentfolder
-#print 'ShowFolder: ', showfolder
+print '################ DEBUG  ######################'
+print 'Provider: ',provider
+print 'Host: ', host
+print 'User: ', user
+print 'Password: ', password
+print 'Database: ', db
+print 'Debug: ', debug
+print 'Server Name: ', server_name
+print 'TorrentFolder: ', torrentfolder
+print 'ShowFolder: ', showfolder
 #os.system('pwd')
 print '\n '
 
@@ -60,27 +73,47 @@ print '\n '
 #list folders only 
 #ls -d */ | sed 's|[/]||g'
 
-for file in os.listdir('/home/ryan/share/'):
+
+for file in os.listdir(torrentfolder):
     filelist.append(file)
  
-
 for f in filelist:
 #    print f +' _____F_____'
     show = f.replace('.',' ')
     episode_code = re.search(regex,show)
 #    print show +'____SHOW____'
     cut_off = (re.search(regex,show))
-    
+
     if episode_code:
-	cut_off = (re.search(regex,show)).start()-1
+	cut_off = (re.search(regex,show)).start()-1 
 	show = show[:cut_off]
 	ecode = episode_code.group()
 	season = ecode[1:-3]
+
+	if season[:1] == '0':
+#	    print 'REMOVE THE LEADING 0'
+	    season = season[-1]
+	    print season
 	episode = ecode[-2:]
 	
-	os.system('ls ~/share/'+f)
+	file = (os.popen('ls '+torrentfolder+'/'+f+' | grep "mkv\|mp4\|avi"').readline()) # needs to be udpated
+
+	file = file.strip()
+	move_files = 'mv -v '+torrentfolder+'/'+f+'/'+file.replace(' ','\ ')+' '+showfolder+'/'+show.replace(' ','\ ')+'/Season\ '+season+'/' # change back to os.system
+	
 	if debug == '1':
-            print show, season, episode
-    else:
-	if debug == '1':
-	    print 'not found'	
+		print move_files
+		print torrentfolder
+
+	os.system(move_files)	# temp - should always try and move
+
+	folder = f.replace(' ','\ ')
+    os.system('rm -rf '+torrentfolder+'/'+f+'/')
+
+#	if debug == '1':
+#            print show, season, episode
+#    else:
+#	if debug == '1':
+#	    print 'not found'	
+
+
